2dd75bbbafaf0ab53d7543abe7b1d59bdeed8233
yosh-0827 <73326181+yosh-0827@users.noreply.github.com>
2021-08-22 21:20:53 +0900

    変数の使い方(const,let,テンプレート文字列）

--- src/index.js
+++ src/index.js
@@ -1,10 +1,68 @@
-import "./styles.css";
+/**
+ * const letなどの変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var vani1 = "var変数";
+// console.log(vani1);
+
+// // var変数は上書き可能
+// vani1 = "var変数はは上書き可能";
+// console.log(vani1);
+
+// // var変数は再宣言可能
+// var vani1 = "var変数を再宣言";
+// console.log(vani1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // let は上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// // エラーがでる
+// let val2 = "let変数を再宣言";
+
+//const val3 = "const変数";
+//console.log(val3);
+
+// const 変数は上書き不可
+// エラーでる
+// val3 = "const変数を上書き";
+
+//const val3 ~ "const変数を再宣言します";
+
+// オブジェクトの場合はCONSTをいじれる
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+// val4.name = "jak";
+// val4.addres = "Hiroshima";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey"); //apeendがpush
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+const name = "じぇけぇ";
+const age = 28;
+//　私の名前はじぇけぇです。年齢は28歳です
+
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+// テンプレート文字列を用いた方法
+// テンプレート文字列はバッククオート「``」を用いる
+// バッククオートはShift + @ でできる
+// 文字列の中でJSを使いたいときは${変数}でいける
+const message2 = `私の名前は${name}です。年齢は${age}です`;
+console.log(message2);
